a7f1dcc7236b031406f49069a64028c9e113522a
Chenye Yang <cy2540@columbia.edu>
2020-09-29 23:49:25 -0400

    update font and color; add Dun Huang photo

--- code/Change_Img_Res.py
+++ code/Change_Img_Res.py
@@ -4,7 +4,7 @@ from PIL import Image
 path_l = '../images/small-images/'
 path_h = '../images/big-images/'
 
-imageFolder = 'background-images/'
+imageFolder = 'experience/'
 
 # read files names
 files = os.listdir(path_h + imageFolder)
@@ -16,5 +16,5 @@ for item in files:
 
 for file in files:
     im_h = Image.open(path_h + imageFolder + file)
-    im_l = im_h.resize((3000, 2000))
+    im_l = im_h.resize((900, 1200))
     im_l.save(path_l + imageFolder + file, quality=85)
